4a0cf67d2f3be15f4c93fbd17bfe2727628bb20a
SAM <19436819+SAM01AA@users.noreply.github.com>
2019-05-28 15:44:19 +0000

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -5,16 +5,17 @@ echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
  if [ $? -eq 0 ]; then
        interface="$(/bin/ip route | awk '$1 == "default" {for (i=2;i<=NF;i++) if ($i == "dev") { i++;print $i; exit}}')"
        if /sbin/iw dev "$interface" info &>/dev/null; then 
-            echo  "wireless connected"     
+            echo  "internet wireless connected"     
 
            
        else 
 
-          echo "lan connected"
+          echo "internet lan connected"
 
        fi
 
  else
+   echo "no internet connection && check after 10min ... please wait"
    sleep 10m
    ( exec "ci.sh" )
 
